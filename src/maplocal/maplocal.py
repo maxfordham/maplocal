eb2efa722a30a18996ee07582a156e930a78a42b
jgunstone <gunstone.john@gmail.com>
2022-12-05 09:57:56 +0000

    updated with core functionality from mf_file_utilities

--- src/maplocal/maplocal.py
+++ src/maplocal/maplocal.py
@@ -1,20 +1,29 @@
 import pathlib
 import typing as ty
 import logging
-#from maplocal.env import MAPENV
 
-def _remove_root(path: pathlib.PurePath, root: pathlib.PurePath) -> ty.Tuple[bool, pathlib.PurePath]:
+from maplocal.env import MapLocalEnv
+MAPENV = MapLocalEnv()
+print('adsf')
+
+def _remove_root(
+    path: pathlib.PurePath, root: ty.Optional[pathlib.PurePath]
+) -> ty.Tuple[bool, pathlib.PurePath]:
     """removes root from path
     Args:
         path (pathlib.PurePath): path
         pathlib (pathlib.PurePath): root to remove from path
     Returns:
-        bool, path: 
+        bool, path:
             bool-> if root successfully removed: True, else: False
             path-> if bool: newpath, else: oldpath
     """
-    assert isinstance(path, pathlib.PurePath), f"path ({path}) passed to _remove_root fn not a valid pathlib.PurePath"
-    assert isinstance(root, pathlib.PurePath), f"root ({root}) passed to _remove_root fn not a valid pathlib.PurePath"
+    assert isinstance(
+        path, pathlib.PurePath
+    ), f"path ({path}) passed to _remove_root fn not a valid pathlib.PurePath"
+    assert isinstance(
+        root, pathlib.PurePath
+    ), f"root ({root}) passed to _remove_root fn not a valid pathlib.PurePath"
     if root in path.parents:
         rootfound = True
         parts = pathlib.Path(path).parts
@@ -25,5 +34,43 @@ def _remove_root(path: pathlib.PurePath, root: pathlib.PurePath) -> ty.Tuple[boo
         newpath = path
     return rootfound, newpath
 
-def maplocal(path: pathlib.Path, oldroot=None, newroot=None):
-    pass
+
+def maplocal(
+    path: pathlib.PurePath,
+    oldroot: ty.Optional[pathlib.PurePath] = None,
+    newroot: ty.Optional[pathlib.PurePath] = None,
+):
+    if oldroot is None:
+        oldroot = MAPENV.MAPLOCAL_FROM
+    if newroot is None:
+        newroot = MAPENV.MAPLOCAL_TO
+    path = pathlib.PurePath(path)
+    rootfound, newpath = _remove_root(path, oldroot)
+    if not rootfound:
+        raise ValueError(f"root: {str(oldroot)}. not found in path: {str(path)}")
+    return newroot / newpath
+
+
+def mapremote(path: pathlib.PurePath,
+    oldroot: ty.Optional[pathlib.PurePath] = None,
+    newroot: ty.Optional[pathlib.PurePath] = None,
+):
+    if oldroot is None:
+        oldroot = MAPENV.MAPLOCAL_TO
+    if newroot is None:
+        newroot = MAPENV.MAPLOCAL_FROM
+    path = pathlib.PurePath(path)
+    rootfound, newpath = _remove_root(path, oldroot)
+    if not rootfound:
+        raise ValueError(f"root: {str(oldroot)}. not found in path: {str(path)}")
+    return newroot / newpath
+
+
+def openlocal(path, mapenv=MAPENV):
+    path = maplocal(path, oldroot=mapenv.MAPLOCAL_FROM, newroot=mapenv.MAPLOCAL_TO)
+
+    return mapenv.openpath(path)
+
+
+def runlocal(cmd, mapenv=MAPENV):
+    return mapenv.runcmd(cmd)
